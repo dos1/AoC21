2e19c832ddc6d33a46ce5c4037f70c521a287f34
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-04 15:20:48 +0100

    Day 4: simplify input parsing

--- day4.py
+++ day4.py
@@ -1,18 +1,10 @@
-SIZE=5
-
-boards=[]
 with open("inputday4") as f:
-  numbers=f.readline().strip().split(',')
-  f.readline()
-  while True:
-    boards.append([[(x, False) for x in f.readline().split()] for i in range(SIZE)])
-    if not f.readline():
-      break
-
+  numbers = f.readline().strip().split(',')
+  boards = [[[(x, False) for x in line.split()] for line in board.split('\n')] for board in f.read().strip().split('\n\n')]
 results = []
 for number in numbers:
   boards = [[[(num[0], num[1] or num[0] == number) for num in line] for line in b] for b in boards]
-  check = lambda b: True in [False not in [b[i][j][1] for j in range(SIZE)] or False not in [b[j][i][1] for j in range(SIZE)] for i in range(SIZE)]
+  check = lambda b: True in [False not in [b[i][j][1] for j in range(5)] or False not in [b[j][i][1] for j in range(5)] for i in range(5)]
   results.extend([sum([int(num[0]) for line in b for num in line if not num[1]]) * int(number) for b in filter(check, boards)])
   boards = filter(lambda x: not check(x), boards)
 print(results[0], results[-1])
