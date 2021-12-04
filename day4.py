8d9dd6f9c8ead18e40a7871ad7e22d0f295573f9
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-04 14:30:35 +0100

    Day 4: get rid of filterfalse
    Let's not use imports.

--- day4.py
+++ day4.py
@@ -1,4 +1,3 @@
-from itertools import filterfalse
 SIZE=5
 
 boards=[]
@@ -15,5 +14,5 @@ for number in numbers:
   boards = [[[(num[0], num[1] or num[0] == number) for num in line] for line in b] for b in boards]
   check = lambda b: True in [False not in [b[i][j][1] for j in range(SIZE)] or False not in [b[j][i][1] for j in range(SIZE)] for i in range(SIZE)]
   results.extend([sum([int(num[0]) for line in b for num in line if not num[1]]) * int(number) for b in filter(check, boards)])
-  boards = filterfalse(check, boards)
+  boards = filter(lambda x: not check(x), boards)
 print(results[0], results[-1])
