0e1adfea03b6c808a76d9325dd1892c69ca3c489
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 08:19:13 +0100

    Day 6: get rid of lambda
    It doesn't add much value there.

--- day6.py
+++ day6.py
@@ -1,6 +1,6 @@
-F,C=[int(val)-8 for val in open("inputday6").read().split(',')],lambda f,d,D:[(n,(D[f],1)[f<0])for n in range(9+f,d,7)]
+F=[int(val)-8 for val in open("inputday6").read().split(',')]
 def count(D):
   for f in F+list(range(len(D))):
-    for n,a in C(f,len(D),D):D[n]+=a
+    for n,a in[(n,(D[f],1)[f<0])for n in range(9+f,len(D),7)]:D[n]+=a
   return len(F)+sum(D)
 print(*(count([0]*(d+1))for d in(80,256)))
