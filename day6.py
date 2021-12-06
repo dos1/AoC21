a2f8447734743eb8f7826d08aceec15def93708a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 10:11:24 +0100

    Day 6: get rid of lambda

--- day6.py
+++ day6.py
@@ -1,3 +1,2 @@
 F=[int(f)-8 for f in open("inputday6").read().split(',')]
-c=lambda D:[D.__setitem__(n,D[n]+a)for f in F+[*range(len(D))]for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7))]and len(F)+sum(D)
-print(c([0]*81),c([0]*257))
+print(*([D.__setitem__(n,D[n]+a)for f in F+[*range(len(D))]for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7))]and len(F)+sum(D)for D in([0]*81,[0]*257)))
