fc5b26a68fb5d0536c223b450f00c6f55c16cfbc
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 10:33:04 +0100

    Day 6: get rid of useless list comprehension

--- day6.py
+++ day6.py
@@ -1,2 +1,2 @@
 F=[int(f)-8 for f in open("inputday6").read().split(',')]
-print(*([D.__setitem__(n,D[n]+a)for f in F+[*range(len(D))]for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7))]and len(F)+sum(D)for D in([0]*81,[0]*257)))
+print(*([D.__setitem__(n,D[n]+(D[f],1)[f<0])for f in F+[*range(len(D))]for n in range(9+f,len(D),7)]and len(F)+sum(D)for D in([0]*81,[0]*257)))
