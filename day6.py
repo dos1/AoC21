1d874a7dce4aa2913c75c4b4cad518dbd28b3319
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 16:21:13 +0100

    Day 6: shorten input reading

--- day6.py
+++ day6.py
@@ -1,2 +1,2 @@
-N,F=257,[int(f)-8 for f in open("inputday6").read().split(',')];D=[0]*N
+N,F=257,[int(f)-8 for f in [*open("inputday6")][0].split(',')];D=[0]*N
 print(*[D.__setitem__(n,D[n]+(D[f],1)[f<0])for f in F+[*range(N)]for n in range(9+f,N,7)]and(len(F)+sum(D[:x])for x in(81,N)))
