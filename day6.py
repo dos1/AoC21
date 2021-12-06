42f859921335718a99bff69496cdd3a68a802198
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 09:56:17 +0100

    Day 6: shorten a bit

--- day6.py
+++ day6.py
@@ -1,6 +1,6 @@
-F=[int(val)-8 for val in open("inputday6").read().split(',')]
-def count(D):
-  for f in F+list(range(len(D))):
-    for n,a in[(n,(D[f],1)[f<0])for n in range(9+f,len(D),7)]:D[n]+=a
+F=[int(f)-8 for f in open("inputday6").read().split(',')]
+def c(D):
+  for f in F+[*range(len(D))]:
+    for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7)):D[n]+=a
   return len(F)+sum(D)
-print(*(count([0]*(d+1))for d in(80,256)))
+print(c([0]*81),c([0]*257))
