3c7e9916d2de2a83580467cf43a7fee081f21101
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-06 10:08:53 +0100

    Day 6: get down to 3 lines

--- day6.py
+++ day6.py
@@ -1,6 +1,3 @@
 F=[int(f)-8 for f in open("inputday6").read().split(',')]
-def c(D):
-  for f in F+[*range(len(D))]:
-    for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7)):D[n]+=a
-  return len(F)+sum(D)
+c=lambda D:[D.__setitem__(n,D[n]+a)for f in F+[*range(len(D))]for n,a in((n,(D[f],1)[f<0])for n in range(9+f,len(D),7))]and len(F)+sum(D)
 print(c([0]*81),c([0]*257))
