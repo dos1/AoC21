06115e4d1c97c882919c2283813e2f6ff5c51bfb
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-07 07:21:59 +0100

    Day 7: don't rewrite crab list into dict of counts

--- day7.py
+++ day7.py
@@ -1,2 +1,2 @@
-*F,=map(int,[*open("inputday7")][0].split(','));C={i:F.count(i)for i in F}
-print(*(min(sum(c(abs(a-i))*C[i]for i in C)for a in range(max(F)+1))for c in[lambda i:i,lambda i:i*(i+1)//2]))
+*F,=map(int,[*open("inputday7")][0].split(','))
+print(*(min(sum(c(abs(a-i))for i in F)for a in range(max(F)+1))for c in[lambda i:i,lambda i:i*(i+1)//2]))
