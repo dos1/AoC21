5916672a34455c8ea611fe25baed0824131faa4c
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-07 07:42:34 +0100

    Day 7: use "int" as identity function

--- day7.py
+++ day7.py
@@ -1,2 +1,2 @@
 *F,=map(int,[*open("inputday7")][0].split(','))
-print(*(min(sum(c(abs(a-i))for i in F)for a in range(max(F)+1))for c in[lambda i:i,lambda i:i*(i+1)//2]))
+print(*(min(sum(c(abs(a-i))for i in F)for a in range(max(F)+1))for c in(int,lambda i:i*(i+1)//2)))
