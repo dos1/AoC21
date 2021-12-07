55eab1ed7e2cc0916f59b97b5a3d6ad8d7483739
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-07 09:48:00 +0100

    Day 7: (z+1) as -~z

--- day7.py
+++ day7.py
@@ -1,2 +1,2 @@
 *F,=map(int,[*open("inputday7")][0].split(','))
-print(*(min(sum((z:=abs(a-i),z*(z+1)//2)[c]for i in F)for a in range(max(F)+1))for c in(0,1)))
+print(*(min(sum((z:=abs(a-i),z*-~z//2)[c]for i in F)for a in range(max(F)+1))for c in(0,1)))
