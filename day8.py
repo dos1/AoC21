38227404015953489a61dfc5a79050871506fef7
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-08 11:28:42 +0100

    Day 8: get rid of useless variable assigment

--- day8.py
+++ day8.py
@@ -1,7 +1,7 @@
 L,R=[[[*map(set,a.split())]for a in l.split('|')]for l in open("inputday8")],{2:1,3:7,4:4,7:8}
 def C(l):
- *d,=map(len,l);S,X=[0]*10,lambda L:[a for a in l if len(a)==L]
- for i in R.keys():S[R[i]]=l[d.index(i)]
+ X,S=lambda L:[a for a in l if len(a)==L],[0]*10
+ for i,j in R.items():S[j]=l[[*map(len,l)].index(i)]
  for e in X(6):S[[[6,0][S[1]&e==S[1]],9][S[4]&e==S[4]]]=e
  for e in X(5):S[[[2,5][e&S[6]==e],3][S[1]&e==S[1]]]=e
  return S
