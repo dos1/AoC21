a190672c59abbce8895de2586c085b99b03216d5
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-08 09:21:22 +0100

    Day 4: get rid of one line

--- day4.py
+++ day4.py
@@ -1,6 +1,5 @@
 numbers, *boards = open("inputday4").read().split('\n\n')
-boards = [[[(x, False) for x in line.split()] for line in board.split('\n')] for board in boards]
-results = []
+boards, results = [[[(x, False) for x in line.split()] for line in board.split('\n')] for board in boards], []
 for number in numbers.split(','):
   boards = [[[(num[0], num[1] or num[0] == number) for num in line] for line in b] for b in boards]
   check = lambda b: True in [False not in [b[i][j][1] for j in range(5)] or False not in [b[j][i][1] for j in range(5)] for i in range(5)]
