cf4f42548c19e5a488e2c2a4dfeb26e6aa9d9f8a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-08 09:14:20 +0100

    Day 8: rebalance line lengths
    No difference in total length.

--- day8.py
+++ day8.py
@@ -1,6 +1,6 @@
-L,z,R=[[[*map(set,a.split())]for a in l.split('|')]for l in open("inputday8")],lambda a,b:a&b==a,{2:1,3:7,4:4,7:8}
+L,R=[[[*map(set,a.split())]for a in l.split('|')]for l in open("inputday8")],{2:1,3:7,4:4,7:8}
 def C(l):
- *d,=map(len,l);S=[0]*10;X=lambda L:[a for a in l if len(a)==L]
+ *d,=map(len,l);S,X,z=[0]*10,lambda L:[a for a in l if len(a)==L],lambda a,b:a&b==a
  for i in R.keys():S[R[i]]=l[d.index(i)]
  for e in X(6):S[[[6,0][z(S[1],e)],9][z(S[4],e)]]=e
  for e in X(5):S[[[2,5][z(e,S[6])],3][z(S[1],e)]]=e
