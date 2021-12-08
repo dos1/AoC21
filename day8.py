e2ec32835ccaba29817a61173cf9bcb1563b8e55
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-08 08:26:25 +0100

    Day 8: reuse the dict of unambiguous lenghts

--- day8.py
+++ day8.py
@@ -1,8 +1,8 @@
-L,z=[[[*map(set,a.split())]for a in l.split('|')]for l in open("inputday8")],lambda a,b:a&b==a
+L,z,R=[[[*map(set,a.split())]for a in l.split('|')]for l in open("inputday8")],lambda a,b:a&b==a,{2:1,3:7,4:4,7:8}
 def C(l):
  *d,=map(len,l);S=[0]*10;X=lambda L:[a for a in l if len(a)==L]
- for i in(a:={2:1,3:7,4:4,7:8}).keys():S[a[i]]=l[d.index(i)]
+ for i in R.keys():S[R[i]]=l[d.index(i)]
  for e in X(6):S[[[6,0][z(S[1],e)],9][z(S[4],e)]]=e
  for e in X(5):S[[[2,5][z(e,S[6])],3][z(S[1],e)]]=e
  return S
-print(sum([*"".join(o:=["".join([str(C(l).index(a))for a in w])for l,w in L])].count(str(i))for i in(1,4,7,8)),sum(map(int,o)))
+print(sum([*"".join(o:=["".join([str(C(l).index(a))for a in w])for l,w in L])].count(str(i))for i in R),sum(map(int,o)))
