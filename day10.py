bd392f0ae0b75b9dfc11aef2d89a5e46fd61ddf4
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-10 10:29:40 +0100

    Day 10: rewrite into 3 lines

--- day10.py
+++ day10.py
@@ -1,9 +1,3 @@
-D,B,s,i=dict(zip(C:='-)]}>',(0,3,57,1197,25137))),'\n([{<',0,[]
-for l in open("inputday10"):
- o=[]
- for c in l:
-  if c in B:o=[C[B.index(c)],*o]
-  elif c==o[0]:o=o[1:]
-  else:s+=D[c];break
- else:i+=[o]
-print(s,sorted((z:=0)or[z:=z*5+C.index(a)for a in c][-1]for c in i)[len(i)//2])
+D,B,s=dict(zip(C:='-)]}>',(0,3,57,1197,25137))),'\n([{<',0
+i=[X for l in open("inputday10")if(X:=(Q:=1,o:=[],W:=[(o.append(C[B.index(c)])if c in B else o.pop()if c==o[-1]else(s:=s+D[c],Q:=0),o)for c in l if Q][-1])[2]and W[1]*Q)]
+print(s,sorted((z:=0)or[z:=z*5+C.index(a)for a in reversed(c)][-1]for c in i)[len(i)//2])
