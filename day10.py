341473a9c4b6a53ab76e78b144f87feffe60fb86
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-10 10:40:34 +0100

    Day 10: construct lists that are already reversed

--- day10.py
+++ day10.py
@@ -1,3 +1,3 @@
 D,B,s=dict(zip(C:='-)]}>',(0,3,57,1197,25137))),'\n([{<',0
-i=[X for l in open("inputday10")if(X:=(Q:=1,o:=[],W:=[(o.append(C[B.index(c)])if c in B else o.pop()if c==o[-1]else(s:=s+D[c],Q:=0),o)for c in l if Q][-1])[2]and W[1]*Q)]
-print(s,sorted((z:=0)or[z:=z*5+C.index(a)for a in reversed(c)][-1]for c in i)[len(i)//2])
+i=[X for l in open("inputday10")if(X:=(Q:=1,o:=[],W:=[((o:=[C[B.index(c)]]+o)if c in B else(o:=o[1:])if c==o[0]else(s:=s+D[c],Q:=0),o)for c in l if Q][-1])[2]and W[1]*Q)]
+print(s,sorted((z:=0)or[z:=z*5+C.index(a)for a in c][-1]for c in i)[len(i)//2])
