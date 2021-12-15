8b8994ead601d5820c429b535b354c9df16f0f5a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-15 14:47:33 +0100

    Day 15: simplify wrapping mapping

--- day15.py
+++ day15.py
@@ -8,4 +8,4 @@ def r(L):
  Q,a=[L[0][0]],lambda i,j:[L[a][b]for a,b in((i-1,j),(i,j-1),(i+1,j),(i,j+1))if(a>=0)*(b>=0)*(a<D(L))*(b<D(L[0]))]
  while Q and(e:=Q.pop()):[i(Q,f,e)for f in a(*e[2])if f[0]>e[0]+e[1]]
  return(x:=L[D(L)-1][D(L[0])-1])[0]+x[1]
-print(*(r([[[(x:=i+j)and float('inf'),x and(s%10+1 if(s:=L[i][j]+n+N)>9 else s),(i+n*D(L),j+N*D(L[0]))]for N in R(l)for j in R(D(L[0]))]for n in R(l)for i in R(D(L))])for l in(1,5)))
+print(*(r([[[(x:=i+j)and float('inf'),x and(L[i][j]+n+N-1)%9+1,(i+n*D(L),j+N*D(L[0]))]for N in R(l)for j in R(D(L[0]))]for n in R(l)for i in R(D(L))])for l in(1,5)))
