bab3f774105857d81cb207a854e05f2f75dd8281
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-16 07:55:46 +0100

    Day 16: shorten

--- day16.py
+++ day16.py
@@ -1,23 +1,22 @@
 d="".join(["0"*(4-len(b:=bin(int(i,16))[2:]))+b for i in open("inputday16").read().strip()])
-consume=lambda d,n:(d[:n],d[n:]);intval=lambda d,n:(int((D:=consume(d,n))[0],2),D[1])
-def mul(v):
+C,I=lambda d,n:(d[:n],d[n:]),lambda d,n:(int((D:=C(d,n))[0],2),D[1])
+def m(v):
  p=1
  for i in v:p*=i
  return p
-def parse(d):
- ver,d=intval(d,3);typeID,d=intval(d,3)
- if typeID==4:
-  number="";prefix,d=intval(d,1)
-  while prefix:num,d=consume(d,4);number+=num;prefix,d=intval(d,1)
-  num,d=consume(d,4);number+=num
-  return int(number,2),ver,d
- op=[sum,mul,min,max,4,lambda x:x[0]>x[1],lambda x:x[0]<x[1],lambda x:x[0]==x[1]][typeID]
- subvalues=[];lengthType,d=intval(d,1)
- if lengthType:
-  length,d=intval(d,11)
-  for i in range(length):val,subsum,d=parse(d);subvalues+=[val];ver+=subsum
+def P(d):
+ v,d=I(d,3);t,d=I(d,3)
+ if t==4:
+  n="";p,d=I(d,1)
+  while p:N,d=C(d,4);n+=N;p,d=I(d,1)
+  N,d=C(d,4);n+=N
+  return int(n,2),v,d
+ S=[];L,d=I(d,1)
+ if L:
+  l,d=I(d,11)
+  for i in range(l):V,Z,d=P(d);S+=[V];v+=Z
  else:
-  length,d=intval(d,15);sub,d=consume(d,length)
-  while len(sub) and int(sub,2):val,subsum,sub=parse(sub);subvalues+=[val];ver+=subsum
- return op(subvalues),ver,d
-print((x:=parse(d))[1],x[0])
+  l,d=I(d,15);s,d=C(d,l)
+  while len(s)and int(s,2):V,Z,s=P(s);S+=[V];v+=Z
+ return[sum,m,min,max,4,lambda x:x[0]>x[1],lambda x:x[0]<x[1],lambda x:x[0]==x[1]][t](S),v,d
+print((x:=P(d))[1],x[0])
