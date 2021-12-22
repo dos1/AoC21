c1a2837174a555e95f11812ea80ce52425cd789f
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-22 07:31:26 +0100

    Day 22: shorten a bit

--- day22.py
+++ day22.py
@@ -4,12 +4,9 @@ lines = [(*[[*map(int,coord.split('..'))] for coord in line[1]], line[0] == "on"
 def countCubes(c):
   if c[0][0] > c[0][1] or c[1][0] > c[1][1] or c[2][0] > c[2][1]: return 0
   return (c[0][1]-c[0][0]+1) * (c[1][1]-c[1][0]+1) * (c[2][1]-c[2][0]+1)
-def intersect(a, b):
-  return tuple((max(a[x][0], b[x][0]), min(a[x][1], b[x][1])) for x in range(3))
-def intersects(a, b):
-  return countCubes(intersect(a,b)) != 0
+def intersect(a, b): return tuple((max(a[x][0], b[x][0]), min(a[x][1], b[x][1])) for x in range(3))
 cuboids = []; fivties = ((-50, 50), (-50, 50), (-50, 50))
 for line in lines:
-  intersections = [*[tuple([*intersect(cuboid, line), not cuboid[3]]) for cuboid in cuboids if intersects(line, cuboid)]]
+  intersections = [*[tuple([*intersect(cuboid, line), not cuboid[3]]) for cuboid in cuboids if countCubes(intersect(line, cuboid))]]
   cuboids += [line, *intersections] if line[3] else intersections
-print(*(sum(countCubes(transform(cuboid)) * [-1,1][cuboid[3]] for cuboid in cuboids) for transform in [lambda x:intersect(x, fivties), lambda x:x]))
+print(*(sum(countCubes(transform(cuboid))*[-1,1][cuboid[3]] for cuboid in cuboids) for transform in [lambda x:intersect(x, fivties), lambda x:x]))
