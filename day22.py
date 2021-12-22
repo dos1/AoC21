b7b24795c716c8eb83c1186c70ad0e872bfe187a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-22 08:54:23 +0100

    Day 22: shorten input parsing

--- day22.py
+++ day22.py
@@ -1,4 +1,4 @@
-L=[(*[[*map(int,c.split('..'))]for c in l[1]],l[0]=="on")for l in [(l[0],[c.split('=')[1]for c in l[1].split(',')])for l in[l.strip().split(' ')for l in open("inputday22")]]]
+L=[(*[[*map(int,c.split('..'))]for c in l],z=="on")for z,l in[(z,[c.split('=')[1]for c in l.split(',')])for z,l in[l.strip().split(' ')for l in open("inputday22")]]]
 C=lambda c:[(c[0][1]-c[0][0]+1)*(c[1][1]-c[1][0]+1)*(c[2][1]-c[2][0]+1),0][c[0][0]>c[0][1]or c[1][0]>c[1][1]or c[2][0]>c[2][1]]
 I,R=lambda a,b:[(max(a[x][0],b[x][0]),min(a[x][1],b[x][1]))for x in[0,1,2]],[]
 for l in L:i=[[*I(c,l),1-c[3]]for c in R if C(I(l,c))];R+=[i,[l,*i]][l[3]]
