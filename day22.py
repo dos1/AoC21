ee8507e18b6ebe0d46d12386e733e8ddb19416e4
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-22 07:20:42 +0100

    Day 22: small input parsing tweaks

--- day22.py
+++ day22.py
@@ -1,6 +1,6 @@
 lines = [line.strip().split(' ') for line in open("inputday22")]
-lines = [(line[0], [line.split('=')[1] for line in line[1].split(',')]) for line in lines]
-lines = [(*[tuple([*map(int,line.split('..'))]) for line in line[1]], line[0] == "on") for line in lines]
+lines = [(line[0], [coord.split('=')[1] for coord in line[1].split(',')]) for line in lines]
+lines = [(*[[*map(int,coord.split('..'))] for coord in line[1]], line[0] == "on") for line in lines]
 def countCubes(c):
   if c[0][0] > c[0][1] or c[1][0] > c[1][1] or c[2][0] > c[2][1]: return 0
   return (c[0][1]-c[0][0]+1) * (c[1][1]-c[1][0]+1) * (c[2][1]-c[2][0]+1)
