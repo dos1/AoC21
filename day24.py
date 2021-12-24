d3760ea2d6a2f3ba94657eb41c926860c40ec519
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-24 14:10:07 +0100

    Day 24: cleanup

--- day24.py
+++ day24.py
@@ -1,11 +1,8 @@
 # This was a very ill-specified and frustrating task. This solution should work with other inputs,
 # but I can't give any guarantee, as inputs aren't really constrained in any way by the task description.
 
-lines = [line.strip().split() for line in open("inputday24")]
-variables = {'w': 0, 'x':0, 'y':0, 'z':0}
-ranges = {}
-
 def analyze(lines):
+  ranges = {}
   i = -1
   for line in lines:
     if line[0] == "inp": i+=1
@@ -15,53 +12,52 @@ def analyze(lines):
         r1 = 1-num
         r2 = 9-num
         ranges[i] = range(min(r1, r2), max(r1,r2) + 1)
+  return ranges
 
-def varOrNum(variables, a):
-  if a in variables.keys():
-    return variables[a]
+def varOrNum(state, a):
+  if a in state.keys():
+    return state[a]
   return int(a)
 
-def run(data, lines, variables):
-  i = 0
-  d = None
-  while lines:
-    line = lines.pop(0)
-    if line[0] == "inp":
+def run(data, lines, state):
+  state = state.copy()
+  for i, line in enumerate(lines):
+    cmd, var = line[:2]
+    if cmd == "inp":
       if not data:
-        lines = [line] + lines
-        break
-      i += 1
-      d = data.pop(0)
-      variables[line[1]] = int(d)
-    elif line[0] == "add":
-      variables[line[1]] += varOrNum(variables, line[2])
-    elif line[0] == "mul":
-      variables[line[1]] *= varOrNum(variables, line[2])
-    elif line[0] == "div":
-      variables[line[1]] //= varOrNum(variables, line[2])
-    elif line[0] == "mod":
-      variables[line[1]] %= varOrNum(variables, line[2])
-    elif line[0] == "eql":
-      variables[line[1]] = int(variables[line[1]] == varOrNum(variables, line[2]))
-  return lines, variables
+        return lines[i:], state
+      state[var] = int(data.pop(0))
+    else:
+      val = varOrNum(state, line[2])
+      if cmd == "add":
+        state[var] += val
+      elif cmd == "mul":
+        state[var] *= val
+      elif cmd == "div":
+        state[var] //= val
+      elif cmd == "mod":
+        state[var] %= val
+      elif cmd == "eql":
+        state[var] = int(state[var] == val)
+  return [], state
 
-results = []
-
-def nextDigit(data, lines, variables, r):
+def nextDigit(data, lines, state, ranges, characterRange):
   if not lines:
-    if variables['z'] == 0:
+    if state['z'] == 0:
       return "".join(map(str,data))
     return False
-  res = []
-  for i in r:
-    lin,var = run([i], lines.copy(), variables.copy())
-    d = data + [i]
-    if len(d) in ranges:
-      if not var['z']%26 in ranges[len(d)]:
+
+  for i in characterRange:
+    newLines, newState = run([i], lines, state)
+    newData = data + [i]
+    if len(newData) in ranges:
+      if not newState['z']%26 in ranges[len(newData)]:
         continue
-    res = nextDigit(d, lin, var, r)
+    res = nextDigit(newData, newLines, newState, ranges, characterRange)
     if res:
       return res
 
-analyze(lines)
-print(nextDigit([], lines, variables, range(9,0,-1)), nextDigit([], lines, variables, range(1,10)))
+lines = [line.strip().split() for line in open("inputday24")]
+initialState = {'w':0, 'x':0, 'y':0, 'z':0}
+ranges = analyze(lines)
+print(nextDigit([], lines, initialState, ranges, range(9,0,-1)), nextDigit([], lines, initialState, ranges, range(1,10)))
