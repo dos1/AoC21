78f2f4e21800145e1d0a7b5b2ea09db254a82495
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-26 10:55:41 +0100

    Day 23: rename some variables for readability

--- day23.py
+++ day23.py
@@ -1,128 +1,128 @@
-lines = (*((*line,) for line in open("inputday23")),)
+data = (*((*line,) for line in open("inputday23")),)
 typeCost = {'A':1, 'B':10, 'C':100, 'D':1000}
 rooms = {'A': 3, 'B':5, 'C':7, 'D':9}
 
-def extend(data):
-  return (*data[:3], (*"  #D#C#B#A#",), (*"  #D#B#A#C#",), *data[3:],)
+def extend(state):
+  return (*state[:3], (*"  #D#C#B#A#",), (*"  #D#B#A#C#",), *state[3:],)
 
-def roomSize(data):
-  return len(data)-3
+def roomSize(state):
+  return len(state)-3
 
-def field(data, x, y):
-  return data[y][x]
+def field(state, x, y):
+  return state[y][x]
 
-def stoppable(data):
-  return tuple(i for i in range(1, len(data[0]) - 1) if i not in rooms.values())
+def stoppableFields(state):
+  return tuple(i for i in range(1, len(state[0]) - 1) if i not in rooms.values())
 
-def isInOwnRoom(data, x, y):
-  if not isAmphipod(data, x, y):
+def isInOwnRoom(state, x, y):
+  if not isAmphipod(state, x, y):
     return False
-  if x == rooms[field(data, x, y)]:
+  if x == rooms[field(state, x, y)]:
     return True
   return False
 
-def isInAnyRoom(data, x, y):
+def isInAnyRoom(state, x, y):
   return y > 1
 
-def isRoomComplete(data, x):
-  for y in range(2,2+roomSize(data)):
-    if not isInOwnRoom(data, x, y):
+def isRoomComplete(state, x):
+  for y in range(2,2+roomSize(state)):
+    if not isInOwnRoom(state, x, y):
       return False
   return True
 
-def roomsComplete(data):
+def roomsComplete(state):
   for room in rooms.values():
-    if not isRoomComplete(data, room): return False
+    if not isRoomComplete(state, room): return False
   return True
 
-def isAmphipod(data, x, y):
-  val = field(data, x, y)
+def isAmphipod(state, x, y):
+  val = field(state, x, y)
   return val if val in rooms.keys() else False
 
-def isEmpty(data, x, y):
-  return field(data, x, y) == '.'
+def isEmpty(state, x, y):
+  return field(state, x, y) == '.'
 
-def isRoomEmpty(data, x):
-  for y in range(2,2+roomSize(data)):
-    if not isEmpty(data, x, y):
+def isRoomEmpty(state, x):
+  for y in range(2,2+roomSize(state)):
+    if not isEmpty(state, x, y):
       return False
   return True
 
-def hasRoomAvailable(data, x, y):
-  amphipod = field(data, x, y)
+def hasRoomAvailable(state, x, y):
+  amphipod = field(state, x, y)
   room = rooms[amphipod]
-  if isRoomEmpty(data, room): return True
-  for y in range(2,2+roomSize(data)):
-    if not isEmpty(data, room, y) and not isInOwnRoom(data, room, y):
+  if isRoomEmpty(state, room): return True
+  for y in range(2,2+roomSize(state)):
+    if not isEmpty(state, room, y) and not isInOwnRoom(state, room, y):
       return False
   return True
 
-def isPathEmpty(data, x, targetX):
+def isPathEmpty(state, x, targetX):
   while x != targetX:
     if x > targetX:
       x-=1
     if x < targetX:
       x+=1
-    if not isEmpty(data, x, 1):
+    if not isEmpty(state, x, 1):
       return False
   return True
 
-def isBlockingRoom(data, x, y):
-  for j in range(y+1, 2+roomSize(data)):
-    if isAmphipod(data, x, j) and not isInOwnRoom(data, x, j):
+def isBlockingRoom(state, x, y):
+  for j in range(y+1, 2+roomSize(state)):
+    if isAmphipod(state, x, j) and not isInOwnRoom(state, x, j):
       return True
   return False
 
-def isBlockedInRoom(data, x, y):
+def isBlockedInRoom(state, x, y):
   if y < 3: return False
-  return not isEmpty(data, x, y-1)
+  return not isEmpty(state, x, y-1)
 
-def moveinPos(data, room):
-  for y in range(1+roomSize(data), 1, -1):
-    if isEmpty(data, room, y):
+def moveinPos(state, room):
+  for y in range(1+roomSize(state), 1, -1):
+    if isEmpty(state, room, y):
       return y
 
-def canMove(data, x, y):
-  return (not isInOwnRoom(data, x, y) or isBlockingRoom(data, x, y)) and not isBlockedInRoom(data, x, y)
+def canMove(state, x, y):
+  return (not isInOwnRoom(state, x, y) or isBlockingRoom(state, x, y)) and not isBlockedInRoom(state, x, y)
 
-def moveCost(data, x, y, i, j):
-  return ((y - 1) + abs(x - i)  + (j - 1)) * typeCost[field(data, x,y)]
+def moveCost(state, x, y, i, j):
+  return ((y - 1) + abs(x - i)  + (j - 1)) * typeCost[field(state, x,y)]
 
 def move(d, x, y, i, j):
   newData = (*((*(((field(d,a,b),field(d,x,y))[a==i and b==j],field(d,i,j))[a==x and b==y] for a in range(len(d[b]))),) for b in range(len(d))),)
   return (newData, moveCost(d, x, y, i, j))
 
-def checkState(data, cache):
-  cached = cache.get(data)
+def checkState(state, cache):
+  cached = cache.get(state)
   if cached is not None:
     return cached
-  if roomsComplete(data):
+  if roomsComplete(state):
     return 0
   costs = []
-  for y in range(1, len(data)):
-    for x in range(1, len(data[y])):
-      amphipod = isAmphipod(data, x, y)
+  for y in range(1, len(state)):
+    for x in range(1, len(state[y])):
+      amphipod = isAmphipod(state, x, y)
       if not amphipod:
         continue
-      if canMove(data, x, y):
+      if canMove(state, x, y):
         room = rooms[amphipod]
-        if hasRoomAvailable(data, x, y) and isPathEmpty(data, x, room):
-          d, c = move(data, x, y, room, moveinPos(data, room))
-          cost = checkState(d, cache)
+        if hasRoomAvailable(state, x, y) and isPathEmpty(state, x, room):
+          newState, newCost = move(state, x, y, room, moveinPos(state, room))
+          cost = checkState(newState, cache)
           if cost >= 0:
-            costs.append(c + cost)
-        elif isInAnyRoom(data, x, y):
-          for i in stoppable(data):
-            if not isPathEmpty(data, x, i):
+            costs.append(cost + newCost)
+        elif isInAnyRoom(state, x, y):
+          for i in stoppableFields(state):
+            if not isPathEmpty(state, x, i):
               continue
-            d, c = move(data, x, y, i, 1)
-            cost = checkState(d, cache)
+            newState, newCost = move(state, x, y, i, 1)
+            cost = checkState(newState, cache)
             if cost >= 0:
-              costs.append(c + cost)
+              costs.append(cost + newCost)
   result = -1
   if costs:
     result = min(costs)
-  cache[data] = result
+  cache[state] = result
   return result
 
-print(checkState(lines, {}), checkState(extend(lines), {}))
+print(checkState(data, {}), checkState(extend(data), {}))
